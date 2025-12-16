cbb96d1a3da236d8e343425bfb6251db3f5dd9e1
OBO4ZZ <obo4zz@naver.com>
2025-12-16 10:58:59 +0900

    PlayerMovement
    .

--- Assets/HyoeunFolder/Sciprts/Player/PlayerController.Movement.cs
+++ Assets/HyoeunFolder/Sciprts/Player/PlayerController.Movement.cs
@@ -16,6 +16,13 @@ public partial class PlayerController
    {
 		m_isGrounded = true;
    }
+   private void MovementUpdate()
+   {
+		Move();
+		Sit();
+		GroundCheck();
+		Jump();
+   }
 
    private void Move()
    {
@@ -24,28 +31,47 @@ public partial class PlayerController
 		m_rigidbody.linearVelocity = new Vector3(h * m_hMoveSpeed, m_rigidbody.linearVelocity.y, m_moveSpeed);
 	}
 
+	private void GroundCheck()
+	{
+		if(!m_isGrounded)
+		{
+			//if()	
+
+			m_isGrounded = true;
+			m_animator.SetBool("Jump", false);
+		}
+	}
 	private void Jump()
 	{
-		if(Input.GetKeyDown(KeyCode.Space))
+		if(Input.GetKeyDown(KeyCode.Space) && m_isGrounded)
 		{
 			m_collider.height = m_collider.height / 2;
-			m_animator.SetBool("Sit", true);
 			m_rigidbody.AddForce(Vector3.up * m_jumpPorce, ForceMode.Impulse);
 
-
+			m_isGrounded = false;
+			m_animator.SetBool("Sit", false);
+			m_animator.SetBool("Jump", true);
 		}
 	}
 	private void Sit()
 	{
 		if (Input.GetKeyDown(KeyCode.LeftShift))
 		{
+			Vector3 scale = m_player.transform.localScale;
+			m_player.transform.localScale = new Vector3(scale.x, scale.y / 1.2f, scale.z);
 			m_collider.height = m_collider.height / 2;
+			m_collider.center = new Vector3(0, m_collider.center.y / 2 , m_collider.center.z);
+
 			m_animator.SetBool("Sit", true);
 		}
 		else if (Input.GetKeyUp(KeyCode.LeftShift)) 
 		{
-			m_collider.height = m_collider.height / 2;
-			m_animator.SetBool("Sit", true);
+			Vector3 scale = m_player.transform.localScale;
+			m_player.transform.localScale = new Vector3(scale.x, scale.y * 1.2f, scale.z);
+			m_collider.height = m_collider.height * 2;
+			m_collider.center = new Vector3(0, m_collider.center.y * 2, m_collider.center.z);
+
+			m_animator.SetBool("Sit", false);
 		}
 
 	}
--- Assets/HyoeunFolder/Sciprts/Player/PlayerController.cs
+++ Assets/HyoeunFolder/Sciprts/Player/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody), (typeof(CapsuleCollider)), (typeof(Animator)))]
 public partial class PlayerController : MonoBehaviour
 {
+	private GameObject m_player;
 	private Rigidbody m_rigidbody;
 	private Animator m_animator;
 	private CapsuleCollider m_collider;
@@ -11,12 +12,13 @@ public partial class PlayerController : MonoBehaviour
 	{
 		m_rigidbody = GetComponent<Rigidbody>();
 		m_animator = GetComponent<Animator>();
+		m_collider = GetComponent<CapsuleCollider>();
+		m_player = this.transform.gameObject;
 
 		MovementAwake();
 	}
 	private void Update()
 	{
-		Move();
-		Jump();
+		MovementUpdate();
 	}
 }
