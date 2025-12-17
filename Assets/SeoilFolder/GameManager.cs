a0b6720a62028f3643a442fde959e48aa6ed8c3d
seoil0404 <seoil6795@gmail.com>
2025-12-17 13:43:37 +0900

    GameManager Added

---
+++ Assets/SeoilFolder/GameManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance {  get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        AudioManager.Instance.SetBGM(AudioManager.Instance.AudioData.MainBGM);
+    }
+}
