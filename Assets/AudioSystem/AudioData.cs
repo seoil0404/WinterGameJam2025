377195c6d7f81483cc1799de656c0f9a58824c73
seoil0404 <seoil6795@gmail.com>
2025-12-17 12:49:12 +0900

    Audio Updated

--- Assets/AudioSystem/AudioData.cs
+++ Assets/AudioSystem/AudioData.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 public class AudioData : ScriptableObject
 {
     [SerializeField] private AudioClip testSound;
+    [SerializeField] private AudioClip eggDestroy;
+    [SerializeField] private AudioClip gameOver;
+    [SerializeField] private AudioClip mainBGM;
+    [SerializeField] private AudioClip abocadoExplosion;
+    [SerializeField] private AudioClip applePeel;
+    [SerializeField] private AudioClip coconutExplosion;
+    [SerializeField] private AudioClip waterMelonExplosion;
 
     public AudioClip TestSound => testSound;
+    public AudioClip EggDestroy => eggDestroy;
+    public AudioClip GameOver => gameOver;
+    public AudioClip MainBGM => mainBGM;
+    public AudioClip AbocadoExplosion => abocadoExplosion;
+    public AudioClip ApplePeel => applePeel;
+    public AudioClip CoconutExplosion => coconutExplosion;
+    public AudioClip WaterMelonExplosion => waterMelonExplosion;
 }
