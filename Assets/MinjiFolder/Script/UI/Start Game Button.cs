1b1a3faeece2a9b4ffefad976612a1c7a3badb14
melhue1218 <zappyfoot1218@gmail.com>
2025-12-18 09:36:10 +0900

    start button

--- Assets/MinjiFolder/Script/UI/Start Game Button.cs
+++ Assets/MinjiFolder/Script/UI/Start Game Button.cs
@@ -3,7 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class StartGameButton : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void StartGame()
     {
         SceneManager.LoadScene("MainScene");
