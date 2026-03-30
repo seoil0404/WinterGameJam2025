29af1fe86c3e0a7daeeeefe118b4c9eed2976548
seoil0404 <seoil6795@gmail.com>
2026-03-30 19:16:41 +0900

    Update SettingSystem

--- Assets/AudioSystem/AudioSettingManager.cs
+++ Assets/AudioSystem/AudioSettingManager.cs
@@ -11,7 +11,6 @@ public class AudioSettingManager : MonoBehaviour
     {
         if(Instance != null)
         {
-            Destroy(gameObject);
             return;
         }
         Instance = this;
--- Assets/SeoilFolder/SettingUI.cs
+++ Assets/SeoilFolder/SettingUI.cs
@@ -5,8 +5,24 @@ public class SettingUI : MonoBehaviour
     public static SettingUI Instance { get; private set; }
     private void Awake()
     {
+        if(Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
         DontDestroyOnLoad(gameObject);
         Instance = this;
         gameObject.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        Time.timeScale = 0f;
+    }
+
+    private void OnDisable()
+    {
+        Time.timeScale = 1f;
     }
 }
