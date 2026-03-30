9a3764286b4a405be29a7c956fc8d46524ff631c
OBO4ZZ <obo4zz@naver.com>
2026-03-30 14:28:18 +0900

    tutorial fixed

--- Assets/HyoeunFolder/Sciprts/TitleUiCanvas.cs
+++ Assets/HyoeunFolder/Sciprts/TitleUiCanvas.cs
@@ -5,5 +5,9 @@ public class TitleUiCanvas : MonoBehaviour
     public void TutorialButton()
     {
         SceneController.Instance.LoadSceneWithoutFade(SceneType.TutorialScene);
-    }
+		PlayerPrefs.SetFloat("TutorialX", 0);
+		PlayerPrefs.SetFloat("TutorialY", 13);
+		PlayerPrefs.SetFloat("TutorialZ", -32.8f);
+		PlayerPrefs.SetInt("TutorialLevel", 0);
+	}
 }
